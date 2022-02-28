dab7b14d79d5124c57a9b0a0cf41936bfdad0ca4
Jonathan Acra <jonathanacra@gmail.com>
2022-02-28 15:58:46 +0000

    cleaned up repo/boilerplate deleted old boilercode

---
+++ src/front/js/pages/GuardianLogin.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+export const GuardianLogin = () => {
+  const { store, actions } = useContext(Context);
+
+  return <div></div>;
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,12 +1,27 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-		},
-		actions: {
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    store: {},
+    actions: {
+      createNewSession: async (email, password) => {
+        const options = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email: email, password: password }),
+        };
+        const response = await fetch(
+          process.env.BACKEND_URL + `/api/token`,
+          options
+        );
+        if (response.status === 200) {
+          const payload = await response.json();
+          localStorage.setItem("session", JSON.stringify(payload));
+          setStore({ session: payload });
+          return payload; //this is gonna make the promise resolve
+          // return jsonify({ "token": access_token, "user_id": user.id })
+        }
+      },
+    },
+  };
 };
 
 export default getState;
