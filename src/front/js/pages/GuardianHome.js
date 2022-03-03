05b35c0ad60610fd1d83b0c22d095c01b2210150
Kennythekaiju <kennethbandera@gmail.com>
2022-03-03 23:03:51 +0000

    guardian components added

--- src/front/js/pages/GuardianHome.js
+++ src/front/js/pages/GuardianHome.js
@@ -2,10 +2,15 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { GuardianNav } from "../component/GuardianNav.jsx";
 
 export const GuardianHome = (props) => {
   const { store, actions } = useContext(Context);
   const params = useParams();
-
-  return <div>GUARDIAN HOME</div>;
+  return (
+    <>
+      <GuardianNav />
+      <div>GUARDIAN HOME</div>;
+    </>
+  );
 };
