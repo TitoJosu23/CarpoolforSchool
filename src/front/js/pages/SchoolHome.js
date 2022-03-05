34215ee6a7179622fc1f22bf20f49260b6648c21
TitoJosu23 <josuearmas00z@gmail.com>
2022-03-05 09:52:22 +0000

    styling the school home

--- src/front/js/pages/SchoolHome.js
+++ src/front/js/pages/SchoolHome.js
@@ -15,17 +15,62 @@ export const SchoolHome = (props) => {
     <div className="SchoolHome">
       <SchoolHomeNavbar />
       <div className="box-modeling">
-        <div className="box box-1">
-          <h3>Search Guardians</h3>
+        <div>
+          <div className="box box-1">
+            <div>
+              <h3>Search Guardians</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                dapibus est eget feugiat molestie. Integer nec dapibus enim, at
+                suscipit lectus. Pellentesque habitant morbi tristique senectus
+                et netus et malesuada fames ac turpis egestas. Vestibulum
+                commodo, nisl eu pretium tristique, ante mi vestibulum lacus,
+                vel aliquam sem mi ac velit. Quisque cursus hendrerit turpis non
+                auctor. Duis quam dui, interdum porttitor sodales in, egestas ac
+                nunc. Aenean vitae lacus mauris.
+              </p>
+              <button className="button button1 left">Press</button>
+              <button className="button button1 right">Press</button>
+            </div>
+          </div>
+          <div className="box box-2">
+            <div>
+              <h3>View Complaints</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                dapibus est eget feugiat molestie. Integer nec dapibus enim, at
+                suscipit lectus. Pellentesque habitant morbi tristique senectus
+                et netus et malesuada
+              </p>
+              <button className="button button3 left">Press</button>
+              <button className="button button3 right">Press</button>
+            </div>
+          </div>
         </div>
-        <div className="box box-2">
-          <h3>Edit School</h3>
-        </div>
-        <div className="box box-3">
-          <h3>View Complaints</h3>
-        </div>
-        <div className="box box-4">
-          <h3>Request</h3>
+        <div>
+          <div className="box box-3">
+            <h3>Edit School</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              dapibus est eget feugiat molestie. Integer nec dapibus enim, at
+              suscipit lectus.
+            </p>
+            <button className="button button2 left">Press</button>
+            <button className="button button2 right">Press</button>
+          </div>
+          <div className="box box-4">
+            <h3>Request</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              dapibus est eget feugiat molestie. Integer nec dapibus enim, at
+              suscipit lectus. Pellentesque habitant morbi tristique senectus et
+              netus et malesuada fames ac turpis egestas. Vestibulum commodo,
+              nisl eu pretium tristique, ante mi vestibulum lacus, vel aliquam
+              sem mi ac velit.
+            </p>
+            <button className="button button4 left">Press</button>
+            <button className="button button4 right">Press</button>
+          </div>
         </div>
       </div>
     </div>
