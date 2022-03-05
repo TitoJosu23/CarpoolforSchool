3123af3ab35cfc3f64cba41a2a2f7c306f01f7ef
Jonathan Acra <jonathanacra@gmail.com>
2022-03-05 21:51:01 +0000

    landing page model almost done

--- src/front/js/pages/LandingPage.js
+++ src/front/js/pages/LandingPage.js
@@ -7,17 +7,22 @@ export const LandingPage = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="landingPage container-fluid d-flex flex-column flex-md-row">
+    <div className="landingPage position-relative container-fluid d-flex flex-column flex-md-row">
       <div className="guardianView position-relative h-100 d-inline-block w-100 w-md-50">
-        <div className="splitText position-absolute top-50 start-50 translate-middle-x bg-dark px-5">
-          <p>Guardian</p>
+        <div className="splitText position-absolute top-50 start-50 translate-middle bg-dark px-5">
+          <Link className="route" to="/guardian/login">
+            <p className="text-center">Guardian</p>
+          </Link>
         </div>
       </div>
-      <div className="position-relative schoolView h-100 d-inline-block w-100 w-md-50">
-        <div className="splitText position-absolute top-50 start-50 translate-middle-x bg-dark px-5">
-          <p className="text-center">School</p>
+      <div className=" schoolView position-relative h-100 d-inline-block w-100 w-md-50">
+        <div className="splitText position-absolute top-50 start-50 translate-middle bg-dark px-5">
+          <Link className="route" to="/school/login">
+            <p className="text-center">Admin</p>
+          </Link>
         </div>
       </div>
+      <div className="centerImage position-absolute top-50 start-50 translate-middle text-dark text-center"></div>
     </div>
   );
 };
