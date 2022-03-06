351b9692fbb694121976ef012b6da51542752de8
Jonathan Acra <jonathanacra@gmail.com>
2022-03-06 20:26:24 +0000

    guardian login in process

--- src/front/js/pages/GuardianLogin.js
+++ src/front/js/pages/GuardianLogin.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
+import "../../styles/guardianLogin.css";
 import { Link } from "react-router-dom";
 
 export const GuardianLogin = () => {
@@ -11,85 +11,55 @@ export const GuardianLogin = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <section className="vh-100" style={{ backgroundColor: "#ACE3E8" }}>
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col col-xl-10">
-            <div className="card" style={{ borderRadius: "1rem" }}>
-              <div className="row g-0">
-                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                  <img
-                    src="https://media.istockphoto.com/photos/stack-of-books-picture-id157482029?k=20&m=157482029&s=612x612&w=0&h=b_0K-1Ut9alhKYu9zlgevSAHDkqSxLF634VDwPw8CHU="
-                    alt="login form"
-                    className="img-fluid"
-                    style={{ borderRadius: "1rem 0 0 1rem" }}
-                  />
-                </div>
-                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                  <div className="card-body p-4 p-lg-5 text-black">
-                    <form
-                      onSubmit={(e) => {
-                        actions
-                          .createNewSession(email, password)
-                          .then((session) => {
-                            history.push("/");
-                          });
-                        e.preventDefault();
-                      }}
-                    >
-                      <div className="d-flex align-items-center mb-3 pb-1"></div>
-                      <h5
-                        className="fw-normal mb-3 pb-3"
-                        style={{ letterSpacing: "1px" }}
-                      >
-                        Sign into your account
-                      </h5>
-                      <div className="form-outline mb-4">
-                        <input
-                          onChange={(e) => setEmail(e.target.value)}
-                          type="email"
-                          id="form2Example17"
-                          className="form-control form-control-lg"
-                        />
-                        <label className="form-label" for="form2Example17">
-                          Email address
-                        </label>
-                      </div>
-                      <div className="form-outline mb-4">
-                        <input
-                          onChange={(e) => setPassword(e.target.value)}
-                          type="password"
-                          id="form2Example27"
-                          className="form-control form-control-lg"
-                        />
-                        <label className="form-label" for="form2Example27">
-                          Password
-                        </label>
-                      </div>
-                      <div className="pt-1 mb-4">
-                        <button className="btn btn-dark btn-lg btn-block">
-                          Login
-                        </button>
-                      </div>
-                      <a className="small text-muted" href="#!">
-                        Forgot password?
-                      </a>
-                      <div className="row mt-4">
-                        <a href="#!" className="small text-muted">
-                          Terms of use.
-                        </a>
-                        <a href="#!" className="small text-muted">
-                          Privacy policy
-                        </a>
-                      </div>
-                    </form>
-                  </div>
-                </div>
+    <div className="guardianLogin container-fluid mx-auto">
+      <div className="container-fluid card mt-5">
+        <div className="container-fluid card-body">
+          <form
+            className="text-center"
+            onSubmit={(e) => {
+              actions.createNewSession(email, password).then((session) => {
+                history.push("/");
+              });
+              e.preventDefault();
+            }}
+          >
+            <h3 className="fw-normal mb-3 pb-3">Sign into your account</h3>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                className="form-control form-control-lg"
+                required
+              />
+              <label className="form-label">Email address</label>
+            </div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                className="form-control form-control-lg"
+                required
+              />
+              <label className="form-label">Password</label>
+            </div>
+            <div className="routeCont container-fluid d-flex justify-content-between">
+              <div className="mt-4">
+                <Link className="route" to="/user/create">
+                  New User?
+                </Link>
+              </div>
+              <div>
+                <p className="btn btn-secondary ms-5">Submit</p>
+              </div>
+              <div className="mt-4">
+                <Link className="route" to="/guardian/reset">
+                  Forgot Password?
+                </Link>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
