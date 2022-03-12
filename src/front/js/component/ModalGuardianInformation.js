2d4df63dc131af56e6dbb281cbfd7cc395558331
TitoJosu23 <josuearmas00z@gmail.com>
2022-03-12 01:59:07 +0000

    doing props in guardian information

--- src/front/js/component/ModalGuardianInformation.js
+++ src/front/js/component/ModalGuardianInformation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/modal.css";
 
-export const ModalGuardianInformation = () => {
+export const ModalGuardianInformation = (props) => {
   return (
     <>
       <div className="guardian-information">
@@ -10,12 +10,13 @@ export const ModalGuardianInformation = () => {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
         <div className="card">
-          <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" />
-          <h1>Guardian Name</h1>
+          <img src="https://www.w3schools.com/w3images/team2.jpg" />
+          <h1>Name: {props.first_name}</h1>
           <p className="title">Children's: (first) (second)</p>
-          <p className="title">Phone Number: +1-234-567-8900</p>
-          <p>Payment Info</p>
-          <a href="https://www.paypal.com/us/home" target="_blank">
+          <p className="title">Phone Number: {props.phone}</p>
+          <p>seats available: {props.seats_available}</p>
+          <p>Payment Info: </p>
+          <a href={props.payment_info} target="_blank">
             <i className="fa fa-paypal"></i>
           </a>
           <p>
