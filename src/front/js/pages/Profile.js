4a48032b00169fe15422a80f6ca6334af7ffc238
TitoJosu23 <josuearmas00z@gmail.com>
2022-03-13 20:51:59 +0000

    modifying profile

--- src/front/js/pages/Profile.js
+++ src/front/js/pages/Profile.js
@@ -4,16 +4,65 @@ import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
 
 export const Profile = (props) => {
+  const [opened, setOpened] = useState(true);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [seats, setSeats] = useState("");
+  const [adress, setAdress] = useState("");
+  const [phone, setPhone] = useState("+1 ");
   return (
     <div className="profile">
       <div>
-        <h2>My Profile</h2>
-        <img src="https://i.pinimg.com/originals/ec/61/d3/ec61d3114cc5269485d508244f531bdf.png" />
-        <p>First name: {props.first_name}</p>
-        <p>Last Name: {props.last_name}</p>
-        <p>Seats Available: {props.seats_available}</p>
-        <p>Address: {props.address}</p>
-        <p>Phone: {props.phone}</p>
+        <h2 className="profile-title">My Profile</h2>
+        <div className="picture">
+          <img src="https://i.pinimg.com/originals/ec/61/d3/ec61d3114cc5269485d508244f531bdf.png" />
+        </div>
+        <div className="profile-information">
+          <p>
+            First name:{" "}
+            <button type="button" className="">
+              edit.
+            </button>
+            {name ? name : ""} {props.first_name}
+          </p>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <p>
+            Last Name: {lastName ? lastName : ""} {props.last_name}
+          </p>
+          <input
+            type="text"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
+          <p>
+            Seats Available: {seats ? seats : ""} {props.seats_available}
+          </p>
+          <input
+            type="text"
+            onChange={(e) => setSeats(e.target.value)}
+            value={seats}
+          />
+          <p>
+            Address: {adress ? adress : ""} {props.address}
+          </p>
+          <input
+            type="text"
+            onChange={(e) => setAdress(e.target.value)}
+            value={adress}
+          />
+          <p>
+            Phone: {phone ? phone : ""} {props.phone}
+          </p>
+          <input
+            type="text"
+            onChange={(e) => setPhone(e.target.value)}
+            value={phone}
+          />
+        </div>
       </div>
     </div>
   );
