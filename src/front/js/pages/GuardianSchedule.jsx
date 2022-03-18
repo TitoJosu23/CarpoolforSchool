535430bfbdbd9be06bf7d1406dfee91779542ae3
TitoJosu23 <josuearmas00z@gmail.com>
2022-03-18 03:56:46 +0000

    doing the schedule model

--- src/front/js/pages/GuardianSchedule.jsx
+++ src/front/js/pages/GuardianSchedule.jsx
@@ -1,78 +1,123 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useHistory } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
-import { Context } from "../store/appContext";
+import React from "react";
+import { Link } from "react-router-dom";
 import { NavReuse } from "../component/NavReuse.jsx";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
-import Switch from "@mui/material/Switch";
-import { AiOutlineCar } from "react-icons/ai";
+
 import "../../styles/guardianSchedule.css";
+
 export const GuardianSchedule = () => {
   return (
-    <>
-      <NavReuse />
-      <div className="form-container">
-        <div className="driving-enabled-toggle">
-          <DriveEnabledSwitch />
-        </div>
-        <div className="time-of-day-container">Morning or Afternoon, both</div>
-        <div className="days-of-week">Mon, Tues, Wed, Thur, Friday</div>
-        <button>Save Settings</button>
-        <button>Change Settings</button>
+    <div className="guardian-schedule">
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitches">
+          Driver
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          readOnly
+        />
       </div>
-    </>
-  );
-};
-
-const DriveEnabledSwitch = () => {
-  const [checked, setChecked] = React.useState([""]);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    {
-      currentIndex === -1
-        ? newChecked.push(value)
-        : newChecked.splice(currentIndex, 1);
-    }
-    setChecked(newChecked);
-  };
-  return (
-    <>
-      <List
-        className="switch-list"
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "#8e9197",
-          color: "#dadce1",
-        }}
-        subheader={<ListSubheader>Drive Settings</ListSubheader>}
-      >
-        <ListItem>
-          <ListItemIcon>
-            <AiOutlineCar
-              style={{ color: "#d2ffff", height: "1.5em", width: "2.5em" }}
-            />
-          </ListItemIcon>
-          <ListItemText id="switch-list-label-drive" primary="Driver" />
-          <Switch
-            style={{ color: "d2ffff" }}
-            edge="end"
-            onChange={handleToggle("drive")}
-            checked={checked.indexOf("drive") !== -1}
-            inputProps={{
-              "aria-labelledby": "switch-list-label-wifi",
-            }}
-          />
-        </ListItem>
-      </List>
-    </>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitchesChecked">
+          Availability
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitches">
+          Morning
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitchesChecked">
+          Afternoon
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitchesChecked"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitches">
+          Monday
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitchesChecked">
+          Tuesday
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitchesChecked"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitches">
+          Wednesday
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitchesChecked">
+          Thursday
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitchesChecked"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitches">
+          Friday
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          readOnly
+        />
+      </div>
+      <div className="custom-control custom-switch box">
+        <label className="custom-control-label" htmlFor="customSwitchesChecked">
+          Accepted
+        </label>
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitchesChecked"
+          readOnly
+        />
+      </div>
+      <input type="submit"/>
+    </div>
   );
 };
